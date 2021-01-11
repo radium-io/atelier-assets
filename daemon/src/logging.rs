8d538a38f04634fb4fa40d4c3e8ca346c600f811
Emory Petermann <emory@radium.io>
2021-01-10 18:30:16 -0600

    fix logging when pretty_log disabled

--- daemon/src/logging.rs
+++ daemon/src/logging.rs
@@ -3,12 +3,37 @@ const DEFAULT_LOGGING_LEVEL: log::LevelFilter = log::LevelFilter::Debug;
 #[cfg(not(debug_assertions))]
 const DEFAULT_LOGGING_LEVEL: log::LevelFilter = log::LevelFilter::Info;
 
+use crate::Result;
+
+/// begins logging with fern, in a nicely formatted way
+#[cfg(feature = "pretty_log")]
+pub fn init_logging() -> Result<()> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{timestamp}][{level}][{target}] {message}",
+                level = record.level(),
+                timestamp = chrono::Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"),
+                target = record.target(),
+                message = message,
+            ))
+        })
+        .chain(std::io::stdout())
+        .level(DEFAULT_LOGGING_LEVEL)
+        .level_for("mio", log::LevelFilter::Info)
+        .level_for("tokio_core", log::LevelFilter::Info)
+        // .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
+
+#[cfg(not(feature = "pretty_log"))]
 use log::{Level, Metadata, Record};
 
-use crate::{Error, Result};
-
+#[cfg(not(feature = "pretty_log"))]
 pub struct SimpleLogger;
 
+#[cfg(not(feature = "pretty_log"))]
 impl log::Log for SimpleLogger {
     fn enabled(&self, metadata: &Metadata<'_>) -> bool {
         metadata.level() <= Level::Info
@@ -23,31 +48,9 @@ impl log::Log for SimpleLogger {
     fn flush(&self) {}
 }
 
-/// begins loggin with fern, in a nicely formatted way
-#[cfg(feature = "pretty_log")]
+#[cfg(not(feature = "pretty_log"))]
 pub fn init_logging() -> Result<()> {
-    if cfg!(feature = "pretty_log") {
-        use chrono::Local;
-        fern::Dispatch::new()
-            .format(|out, message, record| {
-                out.finish(format_args!(
-                    "[{timestamp}][{level}][{target}] {message}",
-                    level = record.level(),
-                    timestamp = Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"),
-                    target = record.target(),
-                    message = message,
-                ))
-            })
-            .chain(std::io::stdout())
-            .level(DEFAULT_LOGGING_LEVEL)
-            .level_for("mio", log::LevelFilter::Info)
-            .level_for("tokio_core", log::LevelFilter::Info)
-            // .chain(fern::log_file("output.log")?)
-            .apply()?;
-        Ok(())
-    } else {
-        log::set_logger(&SimpleLogger)
-            .map(|()| log::set_max_level(DEFAULT_LOGGING_LEVEL))
-            .map_err(Error::SetLoggerError)
-    }
+    log::set_logger(&SimpleLogger)
+        .map(|()| log::set_max_level(DEFAULT_LOGGING_LEVEL))
+        .map_err(crate::Error::SetLoggerError)
 }
