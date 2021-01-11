2077f161551977d1549d2d5e637b64223a99711c
Emory Petermann <emory@radium.io>
2021-01-10 18:32:06 -0600

    skip fmt for generated schemas

--- schema/src/schemas/mod.rs
+++ schema/src/schemas/mod.rs
@@ -1,9 +1,12 @@
 #[allow(clippy::all)]
 #[allow(dead_code)]
+#[rustfmt::skip]
 pub mod data_capnp;
 #[allow(clippy::all)]
 #[allow(dead_code)]
+#[rustfmt::skip]
 pub mod pack_capnp;
 #[allow(clippy::all)]
 #[allow(dead_code)]
+#[rustfmt::skip]
 pub mod service_capnp;
